933f122cc1ca0211be0019afe2cd71827382c9ea
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-08-27 07:01:54 +0300

    Apply container as reuseable css,

--- src/components/layouts/Category.tsx
+++ src/components/layouts/Category.tsx
@@ -3,8 +3,8 @@ import CategoryList from "./CategoryList";
 const Category = () => {
   return (
     <div className="bg-purple-900/5 relative px-5">
-      <div className="pattern-box absolute bottom-0 left-0 opacity-15 w-full h-full"></div>
-      <div className="max-w-[1300px] mx-auto py-20 z-10 relative">
+
+      <div className="container z-10 relative">
         <Title
           title="Category"
           subtitle="Explore our wide range of categories tailored for every gamer’s needs."
