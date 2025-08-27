266067d7583587f298b5f8e1e0c7d5c0e8238f31
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-08-27 17:31:36 +0300

    Adjust design classes for title component

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -7,10 +7,10 @@ interface props {
 const Title = ({ title, subtitle }: props) => {
   return (
     <div className="flex gap-5 items-start">
-      <div className="w-8 xl:h-15 h-10 bg-purple-500 rounded-xl"></div>
+      <div className="w-6 h-12  bg-primary rounded-2xl"></div>
       <div className="flex flex-col gap-5">
         <div className="xl:text-5xl text-4xl">{title}</div>
-        <div className="xl:text-lg text-gray-500">{subtitle}</div>
+        <div className="text-muted-foreground">{subtitle}</div>
       </div>
     </div>
   );
