581555f63d86924ab5c84aedde1ca490b0af964a
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-08-27 07:02:29 +0300

    Append category and products under the same background pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Category from "@/components/layouts/Category";
 import Header from "@/components/layouts/Header";
 import Hero from "@/components/layouts/Hero";
+import Products from "@/components/layouts/Products";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <Category />
+      <div className="relative">
+        <div className="pattern-box absolute bottom-0 left-0 opacity-15 w-full h-full"></div>
+        <Category />
+        <Products />
+      </div>
     </>
   );
 }
