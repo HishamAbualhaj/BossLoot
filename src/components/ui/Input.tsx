6d2b526506e7d81e6f5d3ad1e806378afc0727f4
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-09-18 11:55:39 +0300

    Change the type of onChange function to handle input element change

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 
 type inputProps = {
-  onChange: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
   className?: string;
   type: string;
+  value: string | number;
 };
-const Input = ({ onChange, placeholder, className, type }: inputProps) => {
+const Input = ({
+  onChange,
+  placeholder,
+  className,
+  type,
+  value,
+}: inputProps) => {
   return (
     <input
       onChange={onChange}
+      value={value}
       type={type}
       placeholder={placeholder}
       className={`${className} rounded-md px-3 sm:py-3 py-2 max-sm:text-sm dark:text-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50  pl-4 w-64 dark:bg-secondary/50 bg-gray-200 text-black dark:border-secondary border-gray-300 focus:border-gaming-purple border`}
