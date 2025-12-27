25a3832d1f56451306bf9f0821a38a33bcf240b6
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-27 08:23:09 +0200

    Adjust payment options type

--- src/types/checkout.ts
+++ src/types/checkout.ts
@@ -1,27 +1,24 @@
 export type Method = "credit" | "crypto" | "paypal" | null;
 
 export type PaymentMethod =
-  | {
-      option: Method;
-      data: {
-        visaCardId: string;
-      };
-    }
-  | {
-      option: Method;
-      data: {
-        walletId: string;
-      };
-    }
-  | {
-      option: Method;
-      data: {
-        accountId: string;
-      };
-    };
+  | { option: "credit"; data: { visaCardId: string } }
+  | { option: "paypal"; data: { accountId: string } }
+  | { option: "crypto"; data: { walletId: string } };
 
-export type card = {
-  id: number;
+export type Paypal = {
+  accountId: string;
+  email: string;
+  name: string;
+  date: string;
+};
+
+export type Wallet = {
+  walletId: string;
+  name: string;
+  email: string;
+};
+export type Card = {
+  id: string;
   holder: string;
   lastFourNums: string;
   type: "visa" | "mastercard";
