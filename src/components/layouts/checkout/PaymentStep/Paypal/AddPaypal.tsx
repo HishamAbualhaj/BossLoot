ff03fbd751836f37c063853ac17e7fbfb1c8be5f
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-12-27 08:24:11 +0200

    Create paypal payment option

--- src/components/layouts/checkout/PaymentStep/Paypal/AddPaypal.tsx
+++ src/components/layouts/checkout/PaymentStep/Paypal/AddPaypal.tsx
@@ -1,8 +1,9 @@
-
 const AddPaypal = () => {
   return (
-    <div>AddPaypal</div>
-  )
-}
+    <div className="text-center cursor-pointer dark:hover:bg-muted hover:bg-muted-foreground/20 border border_main rounded-md py-5 mt-5 transition">
+      Connect a paypal account
+    </div>
+  );
+};
 
-export default AddPaypal
+export default AddPaypal;
